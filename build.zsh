4f4a266fa234945a46649d86c6e8d5210c339f66
Russell Bentley <russell.w.bentley@icloud.com>
2017-06-19 21:33:27 -0700

    Misc changes. About to add a new build script

--- build.zsh
+++ build.zsh
@@ -10,6 +10,7 @@ WEB_SRC="web_src"
 WEB_SRC_ROOT="web_src_root"
 RESULT_ROOT="deploy"
 ENV_LIQUID="$WEB_SRC_ROOT/_includes/env.liquid"
+DEPLOY_DIR="../thermalspan.github.io"
 
 if [ -z {1+x} ]
 then
@@ -17,7 +18,8 @@ then
 else
     case $1 in
     deploy)
-        BASE_URL="thermalspan.github.io"
+        DEPLOY=1
+        BASE_URL="https://thermalspan.github.io"
         ;;
     *)
         BASE_URL="$(pwd)/$RESULT_ROOT"
@@ -33,5 +35,10 @@ rm -f $ENV_LIQUID
 echo "{% assign title = \"thermalspan\" %}" >> $ENV_LIQUID
 echo "{% assign base_url = \"$BASE_URL\" %}" >> $ENV_LIQUID
 
+col_echo "Running Cobalt ..." 3
 cobalt build --source $WEB_SRC_ROOT --destination $RESULT_ROOT --config $WEB_SRC_ROOT/.cobalt.yml
+
+col_echo "Dittoing result to deploy dir ..." 3
+rm -rf $DEPLOY_DIR/*
+ditto $RESULT_ROOT $DEPLOY_DIR
 
